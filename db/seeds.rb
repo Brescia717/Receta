733baa262a096bb64f1085cb9ea19b9aecfac6a4
Paul Brescia <brescia717@gmail.com>
2015-02-27 13:31:16 -0500

    Added a seed entry

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Recipe.create(name: "French Toast", instructions: "Toast + Eggs + Pan -> GO!")
